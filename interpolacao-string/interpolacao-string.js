4b6b4c283ed1f0d85c38e24bd6b1001612b81657
Paulo Henrique <Henrique@Paulos-MBP.lan>
2019-08-12 23:05:11 -0300

    interpolacao de string

---
+++ interpolacao-string/interpolacao-string.js
@@ -0,0 +1,9 @@
+// Concatenando strings
+var nome = 'Paulão';
+var mensagem = "Hello, " + nome;
+console.log(mensagem);
+// Operações
+var num1 = 2;
+var num2 = 4;
+var mensagem2 = "Soma: " + (num1 + num2);
+console.log(mensagem2);
---
+++ interpolacao-string/interpolacao-string.ts
@@ -0,0 +1,13 @@
+
+// Concatenando strings
+let nome = 'Paulão';
+let mensagem = `Hello, ${nome}`;
+console.log(mensagem);
+
+
+// Operações
+var num1 = 2;
+var num2 = 4;
+
+let mensagem2 = `Soma: ${num1 + num2}`;
+console.log(mensagem2);
