acb0fb219b1a9a49cfc86b8fe2d8a4fa240cef16
Paulo Henrique <Henrique@Paulos-MBP.lan>
2019-08-13 21:18:42 -0300

    importacao de modulos

---
+++ importacoes-modulos/importacoes-modulos.ts
@@ -0,0 +1,13 @@
+export function some(){}
+export function sub(){}
+export function mult(){}
+export function div(){}
+
+// importa um modulo
+import nome-modulo from 'nome-module';
+
+// importa tudo
+import * as nome-modulo from 'nome-module'
+
+// importa modulo especifico
+import { soma } from 'nome-modulo';
