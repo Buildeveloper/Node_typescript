e4804ebdcaa9b9cc31f14d940f5ea2515f863d68
Paulo Henrique <Henrique@Paulos-MacBook-Pro.local>
2019-08-29 23:25:22 -0300

    config continue integration with trevis

--- server/config/env/test.env.ts
+++ server/config/env/test.env.ts
@@ -2,12 +2,12 @@ module.exports = {
     env: 'test',
     db: 'ts_api_test',
     dialect: 'postgres',
-    username: 'postgres',
+    username: 'Henrique',
     password: 'root',
     host: 'localhost',
     serverPort: 3000,
     pgPort: 5432,
-    dbUrl: 'postgres://postgres:root@localhost:5432/ts_api_test',
+    dbUrl: 'postgres://Henrique:root@localhost:5432/ts_api_test',
     secret: 'S3cr3t'
 };
 
