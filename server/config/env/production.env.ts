c23390a9c477029d57da0b224e8a725ca86d76c0
Paulo Henrique <Henrique@Paulos-MacBook-Pro.local>
2019-08-29 23:29:05 -0300

    config continue integration with trevis

--- server/config/env/production.env.ts
+++ server/config/env/production.env.ts
@@ -2,11 +2,11 @@ module.exports = {
     env: 'production',
     db: 'ts-api',
     dialect: 'postgres',
-    username: 'Henrique',
+    username: 'henrique',
     password: 'root',
     host: 'localhost',
     serverPort: 3000,
     pgPort: 5432,
-    dbUrl: 'postgres://Henrique:root@localhost:5432/ts-api',
+    dbUrl: 'postgres://henrique:root@localhost:5432/ts-api',
     secret: 'S3cr3t'
 };
