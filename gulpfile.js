dda6cf2aa292d622ff5f4a3f3b116420d6bb7915
Paulo Henrique <Henrique@Paulos-MacBook-Pro.local>
2019-08-29 22:13:53 -0300

    config taks runner gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,23 @@ var ts = require('gulp-typescript');
 
 var tsProject = ts.createProject('tsconfig.json');
 
+gulp.task('clean', function() {
+    return gulp.src('dist')
+                .pipe(clean());
+});
+
 gulp.task('compile', function() {
     return tsProject.src()
                     .pipe(tsProject())
                     .js.pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', gulp.series('compile', function(done) { 
+gulp.task('copy-opts', gulp.series('clean','compile', function() {
+    return gulp.src('tests/unit/config/mocha.opts')
+            .pipe(gulp.dest('dist/tests/unit/config'))
+            .pipe(gulp.dest('dist/tests/integration/config'))
+}));
+
+gulp.task('default', gulp.series('copy-opts', function(done) { 
     done();
 }));
